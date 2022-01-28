dc3a48299db59a697596269f3b303156c62f3a15
David Valor <david.valor@geoselfie.com.au>
2022-01-28 15:19:32 -0400

    Added new parameters to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-sshpass -p c0ncentrationLocationActionReview ssh root@143.244.161.159 'cd projects/altyro/Altyro-app && rm -rf /var/www/html/apprepartidores && cp -r www /var/www/html/apprepartidores'
+sshpass -p c0ncentrationLocationActionReview ssh root@143.244.161.159 'cd projects/altyro/Altyro-app && git pull && rm -rf /var/www/html/repar && cp -r www /var/www/html/repar'
