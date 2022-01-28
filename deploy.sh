90fb908952af435590b7f9d77159cb7f2a8ffbe6
David Valor <david.valor@geoselfie.com.au>
2022-01-28 15:28:15 -0400

    Deploying in server

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
+git pull
+ng build
+git add .
+git commit -m "Deploying in server"
+git push
 sshpass -p c0ncentrationLocationActionReview ssh root@143.244.161.159 'cd projects/altyro/Altyro-app && git pull && rm -rf /var/www/html/repar && cp -r www /var/www/html/repar'
